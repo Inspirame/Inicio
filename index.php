3a054810968998f3eaf9af202f2583d671f8db4a
Inspire ME <82292429+Inspirame@users.noreply.github.com>
2021-04-18 20:39:26 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
   <link rel="stylesheet" href="index.css">
 </head>
 <body>
-  <div id="bmenu" align="center">
-    <img src="bannerimg.png" alt="">
+<div id="bmenu" align="center"><br><br><br><br><br>
+    <img src="bannerimg.png.png" width="360px" alt=""><br><br><br><br><br><br>
 <div id="menu"><br>
     <a href="https://inspirame.github.io/Inicio/" id="menu">Inicio</a>
     <a href="https://inspirame.github.io/Tienda/" id="menu">Tienda</a>
