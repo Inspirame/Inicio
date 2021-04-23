643a8d2f7d1d960ca025a7c15555c985a4511ac2
Inspire ME <82292429+Inspirame@users.noreply.github.com>
2021-04-23 15:00:21 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+ <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta charset="UTF-8">
+  <title>Inspire ME</title>
+  <link rel="stylesheet" href="index.css">
+</head>
+<body>
+<div id="bmenu" align="center"><br><br><br><br><br>
+    <img src="bannerimg.png.png" width="360px" alt=""><br><br><br><br><br><br>
+<div id="menu"><br>
+    <a href="https://inspirame.github.io/Inicio/" id="menu">Inicio</a>
+    <a href="https://inspirame.github.io/Tienda/" id="menu">Tienda</a>
+    <a href="https://inspirame.github.io/Contacto/" id="menu">Contacto</a></b>
+</div></div>
+</body>
+</html>
